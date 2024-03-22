1c740426846a011b8ad11d5efd7f34476c26a075
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2024-03-22 14:46:57 +0700

    commit

--- api-server/tx.js
+++ api-server/tx.js
@@ -1,13 +1,13 @@
 const { getCCP } = require("./buildCCP");
 const { Wallets, Gateway } = require('fabric-network');
 const path = require("path");
-const walletPath = path.join(__dirname, "wallet");
+let walletPath
 const {buildWallet} =require('./AppUtils')
 exports.tx = async (request) => {
     let org = request.org;
     let num = Number(org.match(/\d/g).join(""));
     const ccp = getCCP(num);
-
+    walletPath = path.join(__dirname,"wallet", org);
     const wallet = await buildWallet(Wallets, walletPath);
 
     const gateway = new Gateway();
