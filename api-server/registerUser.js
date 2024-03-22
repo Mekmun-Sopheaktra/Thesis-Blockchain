67791dd764b2ca6d7ec5c8601f4c90911d62bea1
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2024-03-22 11:33:04 +0700

    Update tx

--- api-server/registerUser.js
+++ api-server/registerUser.js
@@ -7,9 +7,22 @@ const { getCCP } = require("./buildCCP");
 const path=require('path');
 const walletPath=path.join(__dirname,"wallet")
 exports.registerUser = async ({ OrgMSP, userId }) => {
+    let org;
 
-    let org = Number(OrgMSP.match(/\d/g).join(""));
-    let ccp = getCCP(org)
+    // Determine org based on OrgMSP
+    if (OrgMSP.includes("Org1MSP")) {
+        org = 1;
+    } else if (OrgMSP.includes("Org2MSP")) {
+        org = 2;
+    } else if (OrgMSP.includes("Org3MSP")) {
+        org = 3;
+    } // Add more conditions for other organizations if needed
+
+    if (!org) {
+        throw new Error("Invalid OrgMSP provided.");
+    }
+
+    let ccp = getCCP(org);
     const caClient = buildCAClient(FabricCAServices, ccp, `ca.org${org}.example.com`);
 
     // setup the wallet to hold the credentials of the application user
