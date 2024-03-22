14486c7d09b6d301e751326ba2cf6c0dadc56756
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2024-03-22 14:08:30 +0700

    commit

--- api-server/app.js
+++ api-server/app.js
@@ -18,7 +18,8 @@ app.post("/register",async(req,res)=>{
         let orgMSP=req.body.orgMSP;
         let userId=req.body.userId;
         let orgId=req.body.orgId;
-        let result =await registerUser({orgMSP,userId,orgId});
+        res.send(orgMSP,userId,orgId);
+        let result =await registerUser({OrgMSP:orgMSP,userId:userId,orgId:orgId});
         res.send(result);
 
     } catch (error) {
