cb0458a0bb100b5350834573ef30418d5a20700a
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2024-03-22 11:46:34 +0700

    Fix Error Register

--- api-server/registerUser.js
+++ api-server/registerUser.js
@@ -8,7 +8,7 @@ const path=require('path');
 const walletPath=path.join(__dirname,"wallet")
 exports.registerUser = async ({ OrgMSP, userId }) => {
 
-    let org = OrgMSP;
+    let org = Number(OrgMSP.match(/\d/g).join(""));
     let ccp = getCCP(org)
     const caClient = buildCAClient(FabricCAServices, ccp, `ca.org${org}.example.com`);
 
