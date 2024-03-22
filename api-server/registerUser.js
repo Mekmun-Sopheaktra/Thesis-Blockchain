cc45b43fa236a4d73d3f2a350b451f956b52cfd1
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2024-03-22 13:03:55 +0700

    Update MSP

--- api-server/registerUser.js
+++ api-server/registerUser.js
@@ -8,7 +8,7 @@ const path=require('path');
 const walletPath=path.join(__dirname,"wallet")
 exports.registerUser = async ({ OrgMSP, userId }) => {
 
-    let org = parseInt(OrgMSP);
+    let org = OrgMSP;
 
     let ccp = getCCP(org)
     const caClient = buildCAClient(FabricCAServices, ccp, `ca.org${org}.example.com`);
