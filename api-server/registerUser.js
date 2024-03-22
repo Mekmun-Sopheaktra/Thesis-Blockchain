ab8b5eba00306dd29de9f07d4189f363db804a91
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2024-03-22 11:58:23 +0700

    Fix Error Register

--- api-server/registerUser.js
+++ api-server/registerUser.js
@@ -8,8 +8,7 @@ const path=require('path');
 const walletPath=path.join(__dirname,"wallet")
 exports.registerUser = async ({ OrgMSP, userId }) => {
 
-    let Org1MSP = "Org1MSP"; // Assuming OrgMSP is supposed to be a string containing "Org1MSP"
-    let org = parseInt(Org1MSP.match(/\d+/)[0]);
+    let org = OrgMSP ? parseInt(OrgMSP.match(/\d+/)[0]) : null;
 
     let ccp = getCCP(org)
     const caClient = buildCAClient(FabricCAServices, ccp, `ca.org${org}.example.com`);
