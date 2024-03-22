c773881634bf5e4f50d12f8980051d247fb4e5d3
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2024-03-22 14:17:57 +0700

    commit

--- api-server/registerUser.js
+++ api-server/registerUser.js
@@ -8,7 +8,8 @@ const path=require('path');
 const walletPath=path.join(__dirname,"wallet")
 exports.registerUser = async ({ OrgMSP, userId, orgId }) => {
 
-    let ccp = getCCP(orgId)
+    let org = orgId.match(/\d/g).join("");
+    const ccp = getCCP(org);
     const caClient = buildCAClient(FabricCAServices, ccp, `ca.org${orgId}.example.com`);
 
     // setup the wallet to hold the credentials of the application user
